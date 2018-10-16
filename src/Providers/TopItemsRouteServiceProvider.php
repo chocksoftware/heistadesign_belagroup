862248dd8aa5a216b7435dbf98cf15681b643829
chocksoftware <edgartkeil@outlook.de>
2018-10-16 13:19:38 +0200

    First Commit

---
+++ src/Providers/TopItemsRouteServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HeistaDesign\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class TopItemsRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+        $router->get('topitems', 'HeistaDesign\Controllers\ContentController@showTopItems');
+    }
+}
