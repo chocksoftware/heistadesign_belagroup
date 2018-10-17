e12819894a9e4429011a16150de6ac4d07147925
chocksoftware <edgartkeil@googlemail.com>
2018-10-17 21:47:57 +0200

    Adding Footer

--- src/Providers/HeistaDesignServiceProvider.php
+++ src/Providers/HeistaDesignServiceProvider.php
@@ -13,5 +13,16 @@ class HeistaDesignServiceProvider extends ServiceProvider
 	public function register()
 	{
  
-	}
+    }
+    /**
+	 * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+           $partial->set('footer', 'HeistaDesign::content.ThemeFooter');
+        }, 0);
+        return false;
+    }
 }
