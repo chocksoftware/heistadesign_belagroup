fb926585705d1173ef55dbfc1c1724f1fbec1c85
chocksoftware <edgartkeil@googlemail.com>
2018-10-17 21:18:27 +0200

    Ceres Tutorial Theme Plugins

---
+++ src/Containers/HeistaDesignContainer.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace HeistaDesign\Containers;
+ 
+use Plenty\Plugin\Templates\Twig;
+ 
+class HeistaDesignContainer
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('HeistaDesign::content.Theme');
+    }
+}
